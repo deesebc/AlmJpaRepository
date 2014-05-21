907defddbac3f6e880ad67d9fb60c339fcb21a82
deesebc <daniel@daniel-HP-G62-PC>
2014-05-21 22:23:24 +0200

    añadimos un aspecto que controle las excepciones de los servicios de forma global

---
+++ AlmJpaRepository/src/main/java/es/home/almacen/bbdd/annotation/ExceptionControl.java
@@ -0,0 +1,11 @@
+package es.home.almacen.bbdd.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ExceptionControl {
+}
